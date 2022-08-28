48cddf704c4c19def0744f0edf0e484c548d23be
youssef <youssef266>
2022-08-28 16:20:41 +0200

    sample project for shopping cart

--- shopping_cart/includes/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-//creat connection with database
-$mysqli= new mysqli('localhost','root','Y$1youssef','shopping_cart');
-//check connection 
-// if ($mysqli->connect_error){
-//     die("connection failed:" .$mysqli->connect_erorr);
-// }
-// echo"connected successfully";
